bc01e7652b7cde15b89429f3c44d04f50c27c982
Luzhuo <LZ.Luzhuo@gmail.com>
2022-11-22 23:46:26 +0800

    v1.1.13

--- lib_picture_select/src/main/java/me/luzhuo/lib_picture_select/PictureSelectListener.java
+++
@@ -1,9 +0,0 @@
-package me.luzhuo.lib_picture_select;
-
-import java.util.List;
-
-import me.luzhuo.lib_file.bean.FileBean;
-
-public interface PictureSelectListener {
-    public void onPictureSelect(List<FileBean> selectFiles);
-}
